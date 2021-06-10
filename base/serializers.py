5595299a74e4e5369cd9c2c3f8216c7c3e31ce69
Yousef500 <Kazuya14725@gmail.com>
2021-06-10 06:18:19 +0300

    added registration

--- base/serializers.py
+++ base/serializers.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from .models import Product
 
@@ -12,11 +14,53 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+# class CreateUserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ['first_name', 'username', 'email', 'password', ]
+
+
+class UserSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(read_only=True)
+    _id = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+
+    def get_name(self, obj):
+        name = obj.first_name
+        if name == '':
+            name = obj.email
+        return name
+
+    def get__id(self, obj):
+        return obj.id
+
+    def get_isAdmin(self, obj):
+        return obj.is_staff
+
+    class Meta:
+        model = User
+        fields = ['_id', 'id', 'username', 'email', 'name', 'isAdmin', ]
+
+
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['_id', 'id', 'username', 'email', 'name', 'isAdmin', 'token']
+
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token.access_token)
+
+
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
 
-        data['username'] = self.user.username
-        data['email'] = self.user.email
+        serializer = UserSerializerWithToken(self.user).data
+
+        for k, v in serializer.items():
+            data[k] = v
 
         return data
--- base/views.py
+++ base/views.py
@@ -1,32 +1,22 @@
-from rest_framework.decorators import api_view
-from rest_framework.exceptions import ValidationError
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView, CreateAPIView
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
+from rest_framework.validators import ValidationError
+from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .models import Product
 # Create your views here.
-from .serializers import ProductSerializer, MyTokenObtainPairSerializer
-
-
-@api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        '/api/products/',
-        '/api/products/create/',
-        '/api/products/upload/',
-        '/api/products/<id>>/reviews/',
-        '/api/products/top/',
-        '/api/products/<id>/',
-        '/api/products/delete/<id>/',
-        '/api/products/<update>/<id>/',
-    ]
-    return Response(routes)
+from .serializers import ProductSerializer, MyTokenObtainPairSerializer, UserSerializer, UserSerializerWithToken
 
 
 # @api_view(['GET'])
 # def getProducts(request):
 #     return Response(products)
+
 
 class ProductListAPIView(ListCreateAPIView):
     queryset = Product.objects.all()
@@ -52,6 +42,43 @@ class ProductListAPIView(ListCreateAPIView):
 #             break
 #
 #     return Response(product)
+
+class UserCreateAPIView(CreateAPIView):
+    serializer_class = UserSerializerWithToken
+
+    def perform_create(self, serializer):
+        data = self.request.data
+        password = data['password']
+        username = data['email']
+        hashed_password = make_password(password)
+        try:
+            serializer.save(password=hashed_password, username=username)
+        except:
+            raise ValidationError({"message": 'User with this email already exists'}, code=status.HTTP_400_BAD_REQUEST)
+        # user = User.objects.create(
+        #     first_name=data['name'],
+        #     username=data['email'],
+        #     email=data['email'],
+        #     password=make_password(data['password'])
+        # )
+
+
+class UserProfileAPIView(APIView):
+    # queryset = User.objects.all()
+    # serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+
+class UserListAPIView(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAdminUser]
+
 
 class ProductDetailAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
